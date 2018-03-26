07290a8b76cf0acaa524e4f2241dda4248be9c09
Xitog <damien.gouteux@gmail.com>
2018-03-27 00:31:59 +0200

    More tests

--- corpus.py
+++ corpus.py
@@ -1,6 +1,11 @@
+# standard
 import os
 import os.path
 import json
+import datetime
+
+# external
+import xlwt
 
 class Repository:
 
@@ -10,10 +15,18 @@ class Repository:
         self.num_found = None
         self.num_read = 0
         self.titles = []
+        self.discarded = {}
     
-    def count(self):
+    def count_files(self):
         return len(self.filenames)
 
+    def count_titles(self):
+        return len(self.titles)
+    
+    def load_all(self):
+        for i in range(0, len(self.filenames)):
+            self.load_one(i)
+    
     def load_one(self, num):
         filename = self.filenames[num]
         path = os.path.join(self.path, filename)
@@ -23,18 +36,34 @@ class Repository:
         if self.num_found is None:
             self.num_found = response['numFound']
         elif self.num_found != response['numFound']:
-            raise Exception('numFound not corresponding to the previously found.')
+            # Tolerating +1 or +2 in response
+            if self.num_found != response['numFound'] - 1 and self.num_found != response['numFound'] - 2:
+                print('[ERROR]')
+                print(f'self.num_found = {self.num_found}')
+                print(f'response["numFound"] = {response["numFound"]}')
+                print(f'in doc {filename}')
+                raise Exception('numFound not corresponding to the previously found.')
         # counting
         docs = response['docs']
         self.num_read += len(docs)
         for doc in docs:
-            self.titles.append(Title(doc))
-
+            try:
+                self.titles.append(Title(doc))
+            except KeyError as ke:
+                kes = str(ke)
+                if kes not in self.discarded:
+                    self.discarded[kes] = []
+                self.discarded[kes].append(str(doc['docid']) + ' in ' + filename)
+    
     def __str__(self):
         return 'Repository ' + self.path + ' (' + str(self.num_read) + ')'
 
     def __repr__(self):
         return str(self)
+
+    def discarded_info(self):
+        for key in self.discarded:
+            print(f'{len(self.discarded[key])} because of missing key {key}')
 
 
 class Domain:
@@ -109,14 +138,18 @@ class Title:
     def __init__(self, dic):
         # Atomic values
         self.docid = dic['docid']
-        self.title = dic['title_s']
         self.kind = dic['docType_s']
         self.date = dic['modifiedDateY_i']
+        # Title
+        title_list = dic['title_s']
+        self.title = title_list[0]
         # Lang
         lang_list = dic['language_s']
         if len(lang_list) > 1:
             raise Exception('Too many langs')
         self.lang = lang_list[0]
+        if self.lang != 'fr':
+            raise KeyError('lang')
         # Authors
         author_list = dic['authFullName_s']
         self.authors = []
@@ -143,7 +176,7 @@ class Counter:
         for val in sorted(values.keys()):
             print('    ', val, ' => ', values[val], sep='')
 
-    def count_sum(self, key=''):
+    def count_length(self, key='', threshold=None, export=None):
         sums = {}
         for t in repo.titles:
             val = len(getattr(t, key))
@@ -151,32 +184,65 @@ class Counter:
                 sums[val] = 1
             else:
                 sums[val] += 1
+            #if val == 2 and key == 'title':
+            #    print(getattr(t, key))
         for val in sorted(sums.keys()):
-            print('    ', val, ' => ', sums[val], sep='')
+            if threshold is None or sums[val] >= threshold:
+                print('    ', val, ' => ', sums[val], sep='')
+        if export is not None:
+            if export == 'Excel':
+                wb = xlwt.Workbook()
+                ws = wb.add_sheet(key)
+                ws.write(0, 0, 'lenght of ' + key)
+                ws.write(0, 1, 'number of titles')
+                row = 1
+                for val in sorted(sums.keys()):
+                    ws.write(row, 0, val)
+                    ws.write(row, 1, sums[val])
+                    row += 1
+                wb.save(key + '.xls')
 
+start = datetime.datetime.now()
 
 repo = Repository('corpus-3046-files-2018-02-20-197-Mo')
-print(repo.count())
-repo.load_one(0)
+print('Nb files :', repo.count_files())
+repo.load_all()
+print('Nb titles:', repo.count_titles())
+#LOAD = 1000
+#for i in range(0, LOAD):
+#    repo.load_one(i)
 counter = Counter(repo)
 # Atomic values
-print('\n----------\n')
+print('\n------ Date ------\n')
 counter.count_values('date')
-print('\n----------\n')
+print('\n------ Kind ------\n')
 counter.count_values('kind')
-print('\n----------\n')
+print('\n------ Lang ------\n')
 counter.count_values('lang')
 # Author
-print('\n----------\n')
+print('\n----- Authors ------\n')
 print('    There is', len(Author.ALL_AUTHORS), 'authors.\n')
+nb_authors_by_length = {}
+print('    nb articles (Y) => authors (X) : There is X authors having Y articles')
 for name, author in Author.ALL_AUTHORS.items():
-    if len(author.titles) > 1:
+    nb = len(author.titles)
+    if nb not in nb_authors_by_length:
+        nb_authors_by_length[nb] = 0
+    nb_authors_by_length[nb] += 1
+for nb in sorted(nb_authors_by_length.keys()):
+    print('   ', nb, ':', nb_authors_by_length[nb])
+print()
+for name, author in Author.ALL_AUTHORS.items():
+    if len(author.titles) >= 190:
         print('   ', name, 'has', len(author.titles), 'publications.')
 print()
-print('    nb authors => articles')
-counter.count_sum('authors')
+print('    nb authors (Y) => articles (X) (there is X articles having Y authors)')
+counter.count_length('authors', threshold=6)
+# Title
+print('\n----- Title -----\n')
+counter.count_length('title', export='Excel') # calcul de longueur
 # Domain
-print('\n----------\n')
+print('\n----- Domains -----\n')
 print('    There is', len(Domain.ROOTS), 'roots domain:')
 i = 1
 nb = 0
@@ -184,6 +250,14 @@ for _, dom in Domain.ROOTS.items():
     nb += dom.display(i) #print('    ', i, '. ', dom, sep='')
     i += 1
 print('There is', nb, 'publications linked to the domains.')
+
+end = datetime.datetime.now()
+
+delta = end - start
+
+print(f"Script has ended [{delta} elapsed].")
+
+repo.discarded_info()
 
 # 3046 fichiers
 # 100 enregistrements à chaque fois
@@ -203,4 +277,6 @@ print('There is', nb, 'publications linked to the domains.')
 # UNDEFINED 1
 
 # fr 100
+
+# 298 118 une fois "filtré"
 
