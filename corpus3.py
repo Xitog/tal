e69c9c4e14fe8b5404b873cc56f2fc3b6dda4f69
Xitog <damien.gouteux@gmail.com>
2018-05-23 02:10:36 +0200

    Filter on empty title and duplicates

--- corpus3.py
+++ corpus3.py
@@ -1,7 +1,7 @@
 #===============================================================================
 # Code handling the elaborated corpus for TAL
 # Author : Damien Gouteux
-# Last updated : 08 April 2018
+# Last updated : 22 May 2018
 # Technologies : Python, Excel, XML
 #===============================================================================
 
@@ -12,6 +12,8 @@
 # standard
 import datetime
 import xml.etree.ElementTree as ET
+import zipfile
+import os
 
 # project
 from corpus2 import Title, ExcelFile
@@ -20,46 +22,96 @@ from corpus2 import Title, ExcelFile
 # Classes
 #-------------------------------------------------------------------------------
 
+#
+# Corpus
+#
 class Corpus:
-
-    def __init__(self, filepath):
-        start = datetime.datetime.now()
+    """A corpus is a collection of Title serialized in an xml File.
+       Loading of the xml file is iterative: it is too big for reading it directly.
+    """
+    def __init__(self):
         self.titles = {}
-        #tree = ET.parse(filepath)
-        #root = tree.getroot()
+
+    @staticmethod
+    def load(filepath):
+        corpus = Corpus()
+        start = datetime.datetime.now()
         for event, elem in ET.iterparse(filepath, events=('end',)):
             if event == 'end':
                 if elem.tag == 'notice':
                     t = Title.from_xml(elem)
-                    self.titles[t.docid] = t
+                    corpus.titles[t.docid] = t
                     elem.clear()
         delta = datetime.datetime.now() - start
-        print('Nb titles:', len(self.titles))
+        print('Nb titles:', len(corpus.titles))
         print(f"Loaded in {delta}.")
+        return corpus
+    
+    # Code taken from Repository.dump
+    def save(self, filename, makezip=False):
+        full_filename = 'output_dump_repo' + os.sep + filename + '.xml'
+        outfile = open(full_filename, encoding='utf-8', mode='w')
+        outfile.write('<notices>\n')
+        for title_id in self.titles:
+            title = self.titles[title_id]
+            try:
+                outfile.write(title.to_xml())
+            except Exception as problem:
+                print(problem)
+                print(title.title, title_id)
+        outfile.write('</notices>')
+        outfile.close()
+        if makezip:
+            out = zipfile.ZipFile('output_dump_repo' + os.sep + filename + '_' + output + '.zip', mode='w')
+            out.write(full_filename)
+            out.close()
 
+    def __getitem__(self, key):
+        return self.titles[key]
+
+    def __setitem__(self, key, val):
+        self.titles[key] = val
+    
+    def __len__(self):
+        return len(self.titles)
+    
+    def add_title(self, t):
+        self[t.docid] = t
+    
     def count_titles(self):
         return len(self.titles)
 
 
-if __name__ == '__main__':
+def filter_zero_words_title():
+    corpus_from = Corpus.load(r'.\output_dump_repo\dump.xml')
+    corpus_to = Corpus()
+    nb_double = 0
+    nb_empty = 0
+    all_title_text = []
+    for title_id in corpus_from.titles:
+        title = corpus_from[title_id]
+        if len(title.words) == 0:
+            nb_empty += 1
+            #print(nb_empty, '. (EMPTY) ', title.title, sep='')
+        elif title.title in all_title_text:
+            nb_double += 1
+            #print(nb_double, '. (DOUBLE) ', title.title, sep='')
+        else:
+            corpus_to.add_title(title)
+            all_title_text.append(title.title)
+    print('Saved corpus is:', len(corpus_to))
+    print('Discarded: empty =', nb_empty, 'double =', nb_double)
+    corpus_to.save('corpus_filtered')
 
-    start_time = datetime.datetime.now()
 
-    print('\n----- Starting ------\n')
-
+def basic_statistics():
     # Result file
-
     excel = ExcelFile(name='results3', mode='w')
     
     print('\n----- Loading corpus ------\n')
+    corpus = Corpus.load(r'D:\Autres\dump.xml')
     
-    #corpus = Corpus(r'output_dump_repo\dump.xml')
-    corpus = Corpus(r'D:\Autres\dump.xml')
-    
-    print('\n----- Structures and words ------\n')
-    
-    # Structures and words after :
-
+    print('\n----- Structures and words after : ------\n')
     TAGS = {}
     WORDS = {}
     window = 4
@@ -96,9 +148,14 @@ if __name__ == '__main__':
     print('\n----- Saving result file ------\n')
     
     excel.save()
-    
+
+if __name__ == '__main__':
+    start_time = datetime.datetime.now()
+    print('\n----- Starting ------\n')
+    # action
+    filter_zero_words_title()
+    # end of action
     print('\n----- End -----\n')
-
     delta = datetime.datetime.now() - start_time
-
     print(f"    Script has ended [{delta} elapsed].")
+    
