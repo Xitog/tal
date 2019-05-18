48fae42259e8bbdc320b2975cb6c98314d91a10b
Xitog <damien.gouteux@gmail.com>
2019-05-18 23:43:30 +0200

    [ProjRech] Défini ma problématique et mes axes de travail. Début carte des articles. Sig noun list v2

---
+++ master2/docs/ProjRechM2CarteArticles.vue
@@ -0,0 +1,91 @@
+<!-- Tufts VUE 3.2.2 concept-map (ProjRechM2CarteArticles.vue) 2019-05-18 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Program%20Files%20(x86)/Dev/VUE/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Sat May 18 23:35:59 CEST 2019 by damie_000 on platform Windows 8 6.2 in JVM 1.7.0_21-b11 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built May 23 2013 at 2146 by tomadm on Linux 2.6.18-348.2.1.el5 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="ProjRechM2CarteArticles.vue" created="1558201696770" x="0.0"
+    y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1558215359815"
+        spec="C:\Users\damie_000\Desktop\ProjRechM2CarteArticles.vue"
+        type="1" xsi:type="URLResource">
+        <title>ProjRechM2CarteArticles.vue</title>
+        <property key="File" value="C:\Users\damie_000\Desktop\ProjRechM2CarteArticles.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/ccdf7d84c0a82bda000fb58a2eea5121</URIString>
+    <child ID="6" label="Nouveau N&#x153;ud" layerID="1"
+        created="1558201779081" x="514.0" y="246.0" width="97.0"
+        height="45.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
+        <fillColor>#E4E6D2</fillColor>
+        <strokeColor>#8C8F72</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/ccdf7da6c0a82bda000fb58a35f9d075</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <layer ID="1" label="Layer 1" created="1558201696814" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/ccdf7dafc0a82bda000fb58a0fde33b3</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-14.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Chemin sans nom" created="1558201696753"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B3993333</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/ccdf7db0c0a82bda000fb58ac876e063</URIString>
+            <masterSlide ID="2" created="1558201697214" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/ccdf7db0c0a82bda000fb58afe485196</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1558201711363" x="335.5" y="172.5"
+                    width="129.0" height="55.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/ccdf7db1c0a82bda000fb58a3a5f7357</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1558201711364" x="346.5" y="281.5"
+                    width="107.0" height="37.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/ccdf7db2c0a82bda000fb58a52b0f66d</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1558201711366"
+                    x="373.5" y="384.0" width="53.0" height="32.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/ccdf7db2c0a82bda000fb58acb6f9b1a</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2019-05-18</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>C:\Users\damie_000\Desktop</saveLocation>
+    <saveFile>C:\Users\damie_000\Desktop\ProjRechM2CarteArticles.vue</saveFile>
+</LW-MAP>
